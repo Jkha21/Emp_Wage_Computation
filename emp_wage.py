2db8635fe5c7d49fdb30deb7b54a6dc127f7871c
jkha21 <hkumar3516@gmail.com>
2024-07-23 18:12:10 +0530

    Added Functionality to calculate Daily Wage

--- emp_wage.py
+++ emp_wage.py
@@ -1,13 +1,21 @@
 import random
 
 def main():
-    print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION =====")
-
-    empCheck = random.randint(0, 1)
-    if empCheck == 0:
+    wage_per_hour = 20
+    full_day = 8
+    
+    print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======")
+    print("")
+    
+    emp_check = random.randint(0, 1)
+    if emp_check == 0:
         print("Employee is Present")
+        print(f"The daily wage of Employee is : {wage_per_hour * full_day}")
     else:
         print("Employee is Absent")
+        print("The daily wage of Employee is : 0")
+    
+    print("")
 
 if __name__ == "__main__":
     main()
