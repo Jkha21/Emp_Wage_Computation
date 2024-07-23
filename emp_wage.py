5fbd88afa09e7b5eacebb930138d1cfe523e96c7
jkha21 <hkumar3516@gmail.com>
2024-07-23 20:57:41 +0530

    Adding Functionality of Monthly Wages

--- emp_wage.py
+++ emp_wage.py
@@ -4,6 +4,7 @@ def main():
     wage_per_hour = 20
     full_day = 8
     half_day = 4
+    total_no_of_days = 20
     
     print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======")
     print("")
@@ -12,10 +13,12 @@ def main():
     
     if emp_check == 0:
         print("Employee is Present")
-        print(f"The daily wage of Employee is : {wage_per_hour * full_day}")
+        daily_wage = wage_per_hour * full_day * total_no_of_days
+        print(f"The daily wage of Employee is : {daily_wage}")
     elif emp_check == 1:
         print("Employee is Present but working part time")
-        print(f"The daily wage of Employee is : {wage_per_hour * half_day}")
+        daily_wage = wage_per_hour * half_day * total_no_of_days
+        print(f"The daily wage of Employee is : {daily_wage}")
     else:
         print("Employee is Absent")
         print("The daily wage of Employee is : 0")
