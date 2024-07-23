8d114219338622bdc40d2a115daa7e288bc8f211
jkha21 <hkumar3516@gmail.com>
2024-07-23 18:05:34 +0530

    Added Functionality to check Employee Attendance

--- emp_wage.py
+++ emp_wage.py
@@ -1,1 +1,13 @@
-print("Welcome to Employee Wage Computation Program")
+import random
+
+def main():
+    print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION =====")
+
+    empCheck = random.randint(0, 1)
+    if empCheck == 0:
+        print("Employee is Present")
+    else:
+        print("Employee is Absent")
+
+if __name__ == "__main__":
+    main()
